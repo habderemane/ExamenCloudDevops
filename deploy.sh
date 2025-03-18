3fd12ff4262ade3c9a051e8e4f06dd039c35f2b2
Abderemane <hananeabderemane@gmail.com>
2025-03-18 18:57:04 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-CONTAINER_NAME=" utilisateur/labisi2025"
-IMAGE_NAME=" utilisateur/labisi2025:latest"
+CONTAINER_NAME=" labisi2025"
+IMAGE_NAME=" hanane933r/labisi2025:latest"
 
 # Arrêt du conteneur existant
 sudo docker stop $CONTAINER_NAME || true
